0e1e8d74825059cf6f52c1b31a81c085b1c87adb
Clematins <152311585+Sorbari2016@users.noreply.github.com>
2026-06-29 23:37:56 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import { router } from "./routes/router.js";
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.use("/", router);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
+
+const PORT = 3000;
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Server is running on port ${PORT}`);
+});
