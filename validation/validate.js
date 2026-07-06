91d5ef111006e3d23d9b7ba3b6388b3450133ea4
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2026-07-06 19:50:02 +0100

    Create validation methods to validate post data

---
+++ validation/validate.js
@@ -0,0 +1,32 @@
+import { body } from "express-validator";
+
+const validateName = (fieldName, displayName) => {
+  return body(fieldName)
+    .trim()
+    .notEmpty()
+    .withMessage(`${displayName} is required!`)
+    .matches(/^[A-Za-z0-9\s\-]+$/)
+    .withMessage(
+      `${displayName} can only contain letters, numbers, spaces, or hyphens`,
+    )
+    .isLength({ max: 30 })
+    .withMessage(`${displayName} name shouldnt be more than 30 characters long`)
+    .escape();
+};
+
+const validataCategory = () => {
+  [validateName("categoryName", "Category name")];
+};
+
+const validateProduct = () => {
+  return [
+    validateName("ProductName", "Product name"),
+    body("description")
+      .trim()
+      .isLength({ max: 200 })
+      .withMessage("Product description must not exceed 200")
+      .escape(),
+  ];
+};
+
+export { validataCategory, validateProduct };
