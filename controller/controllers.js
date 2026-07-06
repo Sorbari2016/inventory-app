fdfb1e12c0df4c4b70bdc3bd95d172001421ad7b
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2026-07-06 16:24:02 +0100

    Create logic to get either category edit or add forms

--- controller/controllers.js
+++ controller/controllers.js
@@ -28,6 +28,7 @@ async function getProductForm(req, res) {
 
   if (req.path === "/products/new") {
     return res.render("forms/product", {
+      title: "New Product",
       formTitle: "Add New Product",
       actionRoute: "/products/new",
       submitText: "Create Product",
@@ -42,6 +43,7 @@ async function getProductForm(req, res) {
   console.log(product);
 
   res.render("forms/product", {
+    title: "Edit Product",
     formTitle: "Edit Product",
     actionRoute: `/products/:${product.id}/edit`,
     submitText: "Save Changes",
@@ -50,4 +52,31 @@ async function getProductForm(req, res) {
   });
 }
 
-export { getHomepage, getProductForm };
+// Create method to get the categories edit or add form
+async function getCategoryForm(req, res) {
+  const categories = await getAllCategories();
+
+  if (req.path === "/categories/new") {
+    return res.render("forms/category", {
+      title: "New Category Page",
+      formTitle: "Add New Category",
+      actionRoute: "/categories/new",
+      submitText: "Create Category",
+      categories: categories,
+    });
+  }
+
+  const categoryId = parseInt(req.params.categoryId);
+  const category = categories.find(
+    (category) => category.category_id === categoryId,
+  );
+  res.render("forms/category", {
+    title: "Edit Category page",
+    formTitle: "Edit Category",
+    actionRoute: "/categories/:categoryId/edit",
+    submitText: "Save Changes",
+    category: category,
+  });
+}
+
+export { getHomepage, getProductForm, getCategoryForm };
--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import { getHomepage, getProductForm } from "../controller/controllers.js";
+import {
+  getHomepage,
+  getProductForm,
+  getCategoryForm,
+} from "../controller/controllers.js";
 
 const router = Router();
 
@@ -8,3 +12,5 @@ export { router };
 router.get("/", getHomepage);
 router.get("/products/new", getProductForm);
 router.get("/products/:productId/edit", getProductForm);
+router.get("/categories/new", getCategoryForm);
+router.get("/categories/:categoryId/edit", getCategoryForm);
