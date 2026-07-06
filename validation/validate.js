be16fe16d0e219a0bae1bae86a23e2000ac17c0f
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2026-07-06 22:41:17 +0100

    Add error messages to the forms

--- validation/validate.js
+++ validation/validate.js
@@ -10,7 +10,7 @@ const validateName = (fieldName, displayName) => {
       `${displayName} can only contain letters, numbers, spaces, or hyphens`,
     )
     .isLength({ max: 30 })
-    .withMessage(`${displayName} name shouldnt be more than 30 characters long`)
+    .withMessage(`${displayName} shouldnt be more than 30 characters long`)
     .escape();
 };
 
